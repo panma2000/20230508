0dc076f32245fafa8a9fbb8fadbe9f7402786acb
panma2000 <127073813+panma2000@users.noreply.github.com>
2023-05-15 16:35:10 +0900

    .
    .

---
+++ app.js
@@ -0,0 +1,45 @@
+import express from 'express';
+import axios from 'axios';
+import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+"use strict";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static(path.join(__dirname, 'docs')));
+app.use(express.json());
+app.use(cors());
+
+app.post('/chat', (req, res) => {
+    const prompt = req.body.prompt;
+    try {
+        axios.post(
+            'https://api.openai.com/v1/chat/completions',
+            {
+                "model": "gpt-3.5-turbo",
+                "messages": [{"role": "user", "content": prompt}]
+            },
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer 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'
+                }
+            }  
+        ).then((response) => {
+            res.json(response.data);
+        })
+    } catch (error) {
+        console.log(error.message);
+        res.json({ error: error.message });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(PORT);
+});
--- main.js
+++ main.js
@@ -1,1 +1,21 @@
 import './style.css'
+import axios from 'axios';
+
+window.onload = function () {
+    document.getElementById('chatform').onsubmit = function (e) {
+        e.preventDefault();
+
+        const prompt = document.getElementById('prompt').value;
+        const responseElement = document.getElementById('response');
+
+        axios.post('http://localhost:3000/chat', { prompt: prompt })
+            .then(function (response) {
+                responseElement.innerHTML = response.data.choices[0].message.content;
+            })
+            .catch(function (error) {
+                console.error('Error:', error);
+                responseElement.textContent = 'Error: ' + error.message;
+            });
+    };
+    
+};
