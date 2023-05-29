322f368487aa974e325406b71104fc40ad36bb26
panma2000 <127073813+panma2000@users.noreply.github.com>
2023-05-29 15:40:00 +0900

    .
    .

--- main.js
+++ main.js
@@ -2,13 +2,13 @@ import './style.css'
 import axios from 'axios';
 
 window.onload = function () {
-    document.getElementById('chatform').onsubmit = function (e) {
+    document.getElementById('chatForm').onsubmit = function (e) {
         e.preventDefault();
 
         const prompt = document.getElementById('prompt').value;
         const responseElement = document.getElementById('response');
 
-        axios.post('http://localhost:3000/chat', { prompt: prompt })
+        axios.post('http://http://165.22.102.61:3000/api/chat', { prompt: prompt })
             .then(function (response) {
                 responseElement.innerHTML = response.data.choices[0].message.content;
             })
