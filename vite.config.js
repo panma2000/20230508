a761c96d74b812506c1be3fb97632a5a41f9fa8d
panma2000 <127073813+panma2000@users.noreply.github.com>
2023-05-29 16:02:41 +0900

    .
    .

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
   // 프로젝트가 호스팅되는 GitHub Pages의 저장소 이름으로 경로를 설정합니다.
   // 예를 들어, 저장소 이름이 'my-project'라면 다음과 같이 설정합니다.
-  base: '/20230508/',
+  base: '/',
   build: {
   outDir: 'docs',
   manifest: true, // Enable the generation of the manifest file
